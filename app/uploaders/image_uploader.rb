713375803b742b0caa7dcdec71cb0e88ecbf7ae8
xdothackerx <xdothackerx@gmail.com>
2014-07-11 21:39:09 -0700

    Reformat project index page.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -39,7 +39,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fit => [500, 252]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
